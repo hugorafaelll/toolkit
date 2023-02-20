fe0bb4c2569f2511490d48e58ae33660ada8dc49
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2023-02-20 15:12:50 -0300

    products array

--- backend/products.js
+++ backend/products.js
@@ -4,21 +4,23 @@ const products = [
     name: "iPhone 12 Pro",
     desc: "6.1-inc display",
     price: 999,
-    image: "",
+    image:
+      "https://trocafone.vtexassets.com/arquivos/ids/245266-800-auto?v=638004125054330000&width=800&height=auto&aspect=true",
   },
   {
     id: 2,
     name: "iPhone 13 Pro",
     desc: "6.4-inc display",
     price: 699,
-    image: "",
+    image:
+      "https://m.media-amazon.com/images/I/41K0G9FgD4L.__AC_SY445_SX342_QL70_ML2_.jpg",
   },
   {
     id: 3,
     name: "Galaxy S",
     desc: "6.5-inc display",
     price: 399,
-    image: "",
+    image: "https://m.media-amazon.com/images/I/61pJ2Jl85jL._AC_SL1000_.jpg",
   },
 ];
 
