711f630349650665cc4cb24460172dd1191c9f2e
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2023-02-21 12:36:34 -0300

    redux resolvido apos 5 dias

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { useGetAllProductsQuery } from "../features/productsApi";
 
 function Home() {
-  return <div>Home</div>;
+  const { data, error, isLoading } = useGetAllProductsQuery();
+
+  return (
+    <div className="home-container">
+      {isLoading ? (
+        <p> Loading ...</p>
+      ) : error ? (
+        <p> Ocorreu um erro {error.data}</p>
+      ) : (
+        <>
+          <h2> Products List</h2>
+          <div className="products">
+            {data?.map((product) => (
+              <div className="product" key={product.id}>
+                <h3>{product.name}</h3>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  style={{ width: "200px", height: "200px" }}
+                />
+                <div className="details">
+                  <span>{product.desc}</span>
+                  <span className="price">${product.price}</span>
+                </div>
+                <button> Add to Cart</button>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+
+import ReactDOM from "react-dom";
 import { configureStore } from "@reduxjs/toolkit";
 
 import "./index.css";
 import App from "./App";
 
 import { Provider } from "react-redux";
-
 import productsReducer, { productsFetch } from "./features/productsSlice";
+import { productsApi } from "./features/productsApi";
 
 const store = configureStore({
   reducer: {
     productsReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
 });
 
 store.dispatch(productsFetch());
 
-https://www.youtube.com/watch?v=X3cE7Hatlo8&list=PL63c_Ws9ecIRnNHCSqmIzfsMAYZrN71L6&index=9
-
-
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
