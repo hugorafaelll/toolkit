e0c101d7459a99f2f6d2fbf635fb841f181a76d5
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2023-02-24 11:29:33 -0300

    criando pag card

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,58 @@
 import React from "react";
 
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 function Cart() {
-  return <div>Teste</div>;
+  const cart = useSelector((state) => state.cart);
+
+  return (
+    <div className="cart-container">
+      <h2> Shooping Cart</h2>
+      {cart.cartItems.length === 0 ? (
+        <div className="cart-empty">
+          <p> Seu carrinho esta vazio </p>
+
+          <div className="start-shopping">
+            <Link to="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                className="bi bi-arrow-left"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+                />
+              </svg>
+              <p>Start Shopping </p>
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <>
+          <div className="titles">
+            <h3 className="product-title">Produto</h3>
+            <h3 className="price">Preço</h3>
+            <h3 className="quantity">Quantidade</h3>
+            <h3 className="total">Total</h3>
+          </div>
+          <div className="cart-items">
+            {cart.cartItems?.map((cartItem) => (
+              <div className="cart-item" key={cartItem.id}>
+                <div className="cart-product">
+                  <img src={cartItem.image} alt={cartItem.name} />
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Cart;
