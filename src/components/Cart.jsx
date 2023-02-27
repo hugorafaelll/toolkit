17eb03c912c0c009169c2b7a55730d3f9d492239
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2023-02-27 17:45:52 -0300

    terminado css

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -51,10 +51,14 @@ function Cart() {
                     <button>Remove</button>
                   </div>
                 </div>
-                <div className="cart-product-price">R$:{cartItem.price}</div>
-                <div className="cart-product-quantity">
+                <div className="cart-product-price">R$:  
+                
+                {cartItem.price}</div>
+                <div 
+                className="cart-product-quantity">
                   <button> - </button>
-                  <div className="count"> {cartItem.cartTotalQuantity}</div>
+                  <div className="count"> 
+                  {cartItem.cartTotalQuantity}</div>
                   <button> + </button>
                 </div>
                 <div className="cart-product-total-price">
@@ -70,7 +74,7 @@ function Cart() {
                   <span>SubTotal </span>
                   <span className="amount">R$:{cart.cartTotaAmount}</span>
                       </div>
-                    <p>Taxes and shipping calculated at checkout</p>
+                    <p>Frete calculado no final da Compra </p>
                     <button>Check out </button>
                     <div className="continue-shopping">
             <Link to="/">
@@ -87,7 +91,7 @@ function Cart() {
                   d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
                 />
               </svg>
-              <p>Continue  Shopping </p>
+              <p>  Continue  Shopping </p>
             </Link>
           </div>
                 </div>
