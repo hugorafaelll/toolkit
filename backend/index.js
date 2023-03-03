9f3d66fd2231937764a9ca67ba09d82e86b609c3
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2023-03-03 18:43:17 -0300

    autenticação e backend nao faz sentido

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,13 @@
 const express = require("express"); // iimportando o modulo express do node
 
 const cors = require("cors"); // outra biblioteca de request http entre paginas
-const mongoose = require("mongoose"); // para conectar com mongodb database
-
 const products = require("./products");
 
 const app = express(); // chamando o express para criar as rotas
 
-require('dotenv').config()
-
-
 app.use(express.json()); // para solicitar o texto em forato json
 app.use(cors()); // dar acesso ao nodejs api na aplicação react
+
 
 app.get("/products", (req, res) => {
   // criando caminho raiz
@@ -27,9 +23,3 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-
-mongoose.connect(uri,{
-useNewUrlParser:true,
-useUnifiedTopology:true,
-} ).then(()=> console.log("MongoDb connection successfull..."))
-.catch(()=>console.log("Coneção falhou"))
